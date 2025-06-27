1320bd8c88713f9a9b1b42ef51d8d44f254f754c
raghul.kumar <Raghul.Kumar@i-exceed.com>
2025-06-28 00:26:59 +0530

    Add email templates for password reset and welcome messages
    - Created a new email style file for consistent styling across emails.
    - Implemented a base email template to standardize the structure of emails.
    - Developed a password reset email template with a link for users to reset their passwords.
    - Created a welcome email template to greet new users and encourage them to upload their profile photo.

--- Utils/email.js
+++ Utils/email.js
@@ -1,28 +1,52 @@
 const nodemailer = require("nodemailer");
+const htmltoText = require("html-to-text");
+const pug = require("pug");
 
-const sendEmail = async options =>{
-    // 1 define the transporter using email trap, later in mail gun tool
-
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth:{
-            user: process.env.EMAIL_USERNAME,
-            pass: process.env.EMAIL_PASSWORD
-        }
-    })
-
-    //2  define the email options to be given
-    const mailOptions = {
-        from: 'Raghul Kumar <raghukrk2000@gmail.com>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message
-        // html message later
+exports.module = class Email {
+    constructor(user, url) {
+        this.to = user.email;
+        this.from = `Raghul Kumar <${process.env.EMAIL_FROM}>`;
+        this.url = url;
+        this.firstName = user.name.split(" ")[0];
     }
+    newTransport() {
+        if (process.env.NODE_ENV === 'production') {
+            // Sendgrid
+            return nodemailer.createTransport({
+                service: 'SendGrid',
+                auth: {
+                    user: process.env.SENDGRID_USERNAME,
+                    pass: process.env.SENDGRID_PASSWORD
+                }
+            });
+        }
 
-    // 3 send the email
-    await transporter.sendMail(mailOptions);
+        return nodemailer.createTransport({
+            host: process.env.EMAIL_HOST,
+            port: process.env.EMAIL_PORT,
+            auth: {
+                user: process.env.EMAIL_USERNAME,
+                pass: process.env.EMAIL_PASSWORD
+            }
+        })
+    }
+    async send(template, subject) {
+        // Render the html file based on the pug template
+        const html = pug.renderFile(`${__dirname}/../views/email/${template}.pug`, {
+            firstName: this.firstName,
+            url: this.url,
+            subject
+        })
+        const mailOptions = {
+            from: this.from,
+            to: this.to,
+            subject,
+            html,
+            text: htmltoText.fromString(html),
+        }
+        await this.newTransport().sendMail(mailOptions);
+    }
+    async sendWelcome() {
+        await this.send("welcome", "Welcome to natours family!");
+    }
 }
-
-module.exports = sendEmail;
